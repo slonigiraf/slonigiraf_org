881c9882a9497d6f0e1e7ae8b7442c8acfc80b76
Reshetov Denis <reshetovdenis@gmail.com>
2020-12-02 12:39:13 +0300

    Language selector was added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { Button, Navbar, NavItem, Nav, Container, Row, Col } from "react-bootstrap";
+import { Dropdown, Button, Navbar, NavItem, Nav, Container, Row, Col } from "react-bootstrap";
 import logo from './logo.svg';
 import loader from './loader.svg';
 import { ReactComponent as Uniswap } from './uniswap.svg';
@@ -13,7 +13,30 @@ function MyComponent() {
   const changeLanguage = lng => {
     i18n.changeLanguage(lng);
   };
-  
+  const getCurrentLng = () => i18n.language || window.localStorage.i18nextLng || '';
+
+  const languages = [
+    { code: 'ar', name: 'العربية' },
+    { code: 'bn', name: 'বাংলা' },
+    { code: 'de', name: 'Deutsch' },
+    { code: 'en', name: 'English' },
+    { code: 'es', name: 'Español' },
+    { code: 'fr', name: 'français' },
+    { code: 'hi', name: 'हिन्दी' },
+    { code: 'id', name: 'Bahasa Indonesia' },
+    { code: 'ja', name: '日本語' },
+    { code: 'ko', name: '한국어' },
+    { code: 'pt', name: 'Português' },
+    { code: 'ru', name: 'русский' },
+    { code: 'sw', name: 'Kiswahili' },
+    { code: 'tr', name: 'Türkçe' },
+    { code: 'zh', name: '中文' },
+  ];
+  const codeToName = new Map();
+  languages.map((lng, index) => codeToName.set(lng.code, lng.name));
+  codeToName.set('', "English");
+
+
   return (
     <div className="App" style={{
       display: 'flex',
@@ -33,7 +56,7 @@ function MyComponent() {
         <Row>
 
           <Col md={12} sm={12}>
-          {/* <button onClick={() => changeLanguage('en')}>en</button> */}
+            {/* <button onClick={() => changeLanguage('en')}>en</button> */}
             <a href="https://slon-i-giraf.ru/app/work?view=findTeacherView&knowledgeCourse=77&utm_content=1606559945976"><Button variant="primary" size="lg">{t('Join us for free')}</Button></a>
             <a href="https://info.uniswap.org/pair/0x2B8795475a61C85636E1Ee13fd21ca1B7C860245"><Button variant="secondary" size="lg"> <Uniswap />&nbsp;{t('Swap SLON/ETH')}</Button></a>
           </Col>
@@ -45,6 +68,14 @@ function MyComponent() {
             <a href="https://t.me/slonigiraf">{t('Follow us on Telegram')}, </a>
             <a href="https://wa.me/79099319358">{t('WhatsApp')}, </a>
             <a href="mailto:info@slon-i-giraf.ru">{t('Email')}</a>
+            <Dropdown>
+              <Dropdown.Toggle variant="link" id="dropdown-basic">
+                {codeToName.get(getCurrentLng())}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                {languages.map((lng, index) => <Dropdown.Item value={lng.code} onClick={() => changeLanguage(lng.code)}>{lng.name}</Dropdown.Item>)}
+              </Dropdown.Menu>
+            </Dropdown>
           </Col>
         </Row>
       </Container>
