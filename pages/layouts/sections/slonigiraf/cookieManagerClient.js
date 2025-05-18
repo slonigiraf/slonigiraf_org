13cc5c43f73b4f3a813726c91b91a0c952d85a41
Reshetov Denis <reshetovdenis@gmail.com>
2025-05-18 20:51:11 +0200

    Updated scripts

--- pages/layouts/sections/slonigiraf/cookieManagerClient.js
+++ pages/layouts/sections/slonigiraf/cookieManagerClient.js
@@ -9,35 +9,53 @@ const CookieManager = dynamic(
 );
 
 const loadAnalytics = () => {
-  // Google Analytics
-  const gaScript = document.createElement('script');
-  gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-Q8JF7VWSM1';
-  gaScript.async = true;
-  document.head.appendChild(gaScript);
+  // Prevent duplicate GA load
+  if (!document.querySelector('script[src*="googletagmanager.com/gtag/js"]')) {
+    const gaScript = document.createElement('script');
+    gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-Q8JF7VWSM1';
+    gaScript.async = true;
+    document.head.appendChild(gaScript);
+  }
 
-  gaScript.onload = () => {
+  const inlineScript = document.createElement('script');
+  inlineScript.innerHTML = `
     window.dataLayer = window.dataLayer || [];
-    function gtag() { window.dataLayer.push(arguments); }
+    function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-Q8JF7VWSM1');
-  };
+  `;
+  document.head.appendChild(inlineScript);
 
-  // Yandex Metrika
+  // Load Yandex Metrika
+  const existingYM = Array.from(document.scripts).find(s => s.src === 'https://mc.yandex.ru/metrika/tag.js');
+  if (existingYM) return;
+
   const ymScript = document.createElement('script');
+  ymScript.type = 'text/javascript';
   ymScript.innerHTML = `
     (function(m,e,t,r,i,k,a){
       m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
       m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) return;
+      }
       k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
     })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
     ym(101962692, "init", {
       clickmap:true,
       trackLinks:true,
-      accurateTrackBounce:true
+      accurateTrackBounce:true,
+      webvisor:true
     });
   `;
   document.head.appendChild(ymScript);
+
+  const noscript = document.createElement('noscript');
+  noscript.innerHTML = `
+    <div><img src="https://mc.yandex.ru/watch/101962692" style="position:absolute; left:-9999px;" alt="" /></div>
+  `;
+  document.body.appendChild(noscript);
 };
 
 const CookieManagerClient = () => {
