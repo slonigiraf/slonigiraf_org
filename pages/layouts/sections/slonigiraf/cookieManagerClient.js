b9051a77a7e5bdd997f44fd5c514d2d16a8996e7
Reshetov Denis <reshetovdenis@gmail.com>
2025-05-18 20:49:57 +0200

    Should work

--- pages/layouts/sections/slonigiraf/cookieManagerClient.js
+++ pages/layouts/sections/slonigiraf/cookieManagerClient.js
@@ -8,6 +8,38 @@ const CookieManager = dynamic(
   { ssr: false }
 );
 
+const loadAnalytics = () => {
+  // Google Analytics
+  const gaScript = document.createElement('script');
+  gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-Q8JF7VWSM1';
+  gaScript.async = true;
+  document.head.appendChild(gaScript);
+
+  gaScript.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { window.dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-Q8JF7VWSM1');
+  };
+
+  // Yandex Metrika
+  const ymScript = document.createElement('script');
+  ymScript.innerHTML = `
+    (function(m,e,t,r,i,k,a){
+      m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(101962692, "init", {
+      clickmap:true,
+      trackLinks:true,
+      accurateTrackBounce:true
+    });
+  `;
+  document.head.appendChild(ymScript);
+};
+
 const CookieManagerClient = () => {
   return (
     <CookieManager
@@ -17,10 +49,20 @@ const CookieManagerClient = () => {
       }}
       displayType="modal"
       theme="light"
+      privacyPolicyUrl="/privacy-policy"
       onSave={(cookies) => {
         console.log('Saved cookie preferences:', cookies);
       }}
-      privacyPolicyUrl="/privacy-policy"
+      onAccept={(cookies) => {
+        console.log('Accepted cookies:', cookies);
+        loadAnalytics();
+      }}
+      onDecline={() => {
+        console.log('Declined cookies');
+      }}
+      onManage={() => {
+        console.log('User opened cookie manager');
+      }}
     />
   );
 };
